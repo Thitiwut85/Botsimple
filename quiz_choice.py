57fd692f5e6d42ede5321478faab4f24e4f87281
Thitiwut11 <73686921+Thitiwut85@users.noreply.github.com>
2020-12-10 14:44:06 +0700

    Add files via upload

---
+++ quiz_choice.py
@@ -0,0 +1,13 @@
+emojinum = {1:"1️⃣", 2:"2️⃣", 3:"3️⃣", 4:"4️⃣", 5:"5️⃣", 6:"6️⃣", 7:"7️⃣", 8:"8️⃣", 9:"9️⃣", 10:"🔟"}
+allquiz = {"1": ("What was the name of the Ethiopian Wolf before they knew it was related to wolves?", 
+                ("Simien Jackel", True), 
+                ("Ethiopian Coyote", False), 
+                ("Amharic Fox", False), 
+                ("Canis Simiensis", False), 
+                {'True':"1️⃣"}), 
+            "2": ("What scientific suborder does the family Hyaenidae belong to", 
+                ("Haplorhini", False), 
+                ("Feliformia", True), 
+                ("Ciconiiformes", False),
+                ("Caniformia", False),
+                {'True':"2️⃣"})}
